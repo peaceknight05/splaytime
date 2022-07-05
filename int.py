edda2007f844a05d67ff6fb0fdfbaca81913f898
Jonathan Tan Jiayi <jonathantanatlol@gmail.com>
2022-07-05 15:50:00 +0800

    fix: bugs with end of input breaking change: changed default behaviour

--- int.py
+++ int.py
@@ -30,7 +30,7 @@ while pc < len(inst):
 	char = inst[pc]
 
 	if char == '{': # insert
-		s, key, value, comma, neg = pc, 0, 0, 0, 1
+		s, key, value, comma, neg, kdefault, vdefault = pc, t.root.key, 0, 0, 1, True, True
 		pc += 1
 		if pc < len(inst): # edge case: entire instruction string is just '{'
 			char = inst[pc]
@@ -40,48 +40,74 @@ while pc < len(inst):
 						sys.stderr.write("Invalid indexing at character %i\n"%pc)
 						sys.exit(1)
 
-					n, addr = 1, 0
-					pc += 1; char = inst[pc]
-					if char == '-' and pc+1 < len(inst) and inst[pc+1].isdigit():
-						n = -1; pc += 1; char = inst[pc]
-					while char.isdigit():
-						addr = addr * 10 + int(char)
-
-						pc += 1
-						if pc >= len(inst): break
+					n, addr, idefault = 1, t.root.key, True
+					pc += 1
+					if pc < len(inst): # edge case: entire instruction string is just {[ or {x|[
 						char = inst[pc]
+						if char == '-' and pc+1 < len(inst):
+							if inst[pc+1].isdigit():
+								n = -1; pc += 1; char = inst[pc]
+							elif inst[pc+1] == '-':
+								sys.stderr.write("Double negative found at character %i"%pc)
+								sys.exit(1)
+						while char.isdigit():
+							if idefault:
+								addr = int(char)
+								idefault = False
+							else: addr = addr * 10 + int(char)
+
+							pc += 1
+							if pc >= len(inst): break
+							char = inst[pc]
 
 					addr *= n
-					data = t.search(addr).data
-					if data == None:
+					node = t.search(addr)
+					if node == None:
 						sys.stderr.write("No node with address %i\n"%addr)
 						sys.exit(1)
+					data = node.data
 
 					if not comma:
 						key = data
+						pc -= 1
+						char = inst[pc]
 					else:
 						value = data
 						break
 				elif char.isdigit():
 					if not comma:
-						key = key*10 + int(char)
+						if kdefault:
+							key = int(char)
+							kdefault = False
+						else: key = key*10 + int(char)
 					else:
-						value = value*10 + int(char)
+						if vdefault:
+							value = int(char)
+							vdefault = False
+						else: value = value*10 + int(char)
 				elif char == '|':
 					if comma:
 						sys.stderr.write("Second '|' found at character %i, one (1) expected\n"%pc)
 						sys.exit(1)
 					comma = pc
+					key *= neg
+					neg = 1
 				elif char == '-':
-					if pc+1 < len(inst) and inst[pc+1].isdigit(): neg *= -1
+					if pc+1 < len(inst):
+						if inst[pc+1].isdigit(): neg = -1
+						elif inst[pc+1] == '-':
+							sys.stderr.write("Double negative found at character %i"%pc)
+							sys.exit(1)
 					else: break
 
 				pc += 1
 				if pc >= len(inst): break
 				char = inst[pc]
 
-		value += 1 if char == '+' else (-1 if char == '-' else 0)
-		value *= neg
+		if comma:
+			value *= neg
+			value += 1 if char == '+' else (-1 if char == '-' else 0)
+		else: key *= neg
 		t.insert((key, value))
 		if args.verbose: print(f"Inserted ({key}, {value})")
 		pc -= 0 if char in '-+' else 1; char = inst[pc] # stops on the character after the numeral, next cycle would skip it
@@ -107,14 +133,19 @@ while pc < len(inst):
 							sys.stderr.write("Invalid indexing at character %i\n"%pc)
 							sys.exit(1)
 
-						saddr = 0
-						pc += 1; char = inst[pc]
-						while char.isdigit():
-							saddr = saddr * 10 + int(char)
-
-							pc += 1
-							if pc >= len(inst): break
+						default, saddr = True, t.root.key
+						pc += 1
+						if pc < len(inst): # edge case: entire instruction string is just '@['
 							char = inst[pc]
+							while char.isdigit():
+								if default:
+									saddr = int(char)
+									default = False
+								saddr = saddr * 10 + int(char)
+
+								pc += 1
+								if pc >= len(inst): break
+								char = inst[pc]
 
 						addr = t.search(saddr).data
 						break
@@ -136,20 +167,26 @@ while pc < len(inst):
 		s, addr = pc, 0
 		pc += 1; char = inst[pc]
 		if pc < len(inst): # edge case: entire instruction string is just '$'
+			char = inst[pc]
 			while (char.isdigit() or char == '['):
 				if char == '[': # read from node
 					if pc != s+1:
 						sys.stderr.write("Invalid indexing at character %i\n"%pc)
 						sys.exit(1)
 
-					saddr = 0
-					pc += 1; char = inst[pc]
-					while char.isdigit():
-						saddr = saddr * 10 + int(char)
-
-						pc += 1
-						if pc >= len(inst): break
+					default, saddr = True, t.root.key
+					pc += 1
+					if pc < len(inst): # edge case: entire instruction string is just '$['
 						char = inst[pc]
+						while char.isdigit():
+							if default:
+								saddr = int(char)
+								default = False
+							saddr = saddr * 10 + int(char)
+
+							pc += 1
+							if pc >= len(inst): break
+							char = inst[pc]
 
 					addr = t.search(saddr).data
 					break
@@ -160,7 +197,7 @@ while pc < len(inst):
 				if pc >= len(inst): break
 				char = inst[pc]
 
-			pc -= 1 # stops on the character after the numeral, next cycle would skip it
+		pc -= 1 # stops on the character after the numeral, next cycle would skip it
 
 		if t.search(addr) == None and args.show_warnings:
 			sys.stderr.write("No node with address %i\n"%addr)
